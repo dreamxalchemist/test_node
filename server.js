b8332d093f8ae85fee319aaf13446acb24d59980
Fernando Beltran <fernando.beltran@somoscmi.com>
2020-10-01 23:10:14 -0600

    test for video

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('It\'s time to sum! ' + sum(14,7));
+  res.send('It\'s time to sum! ' + sum(14,14));
 });
 
 app.listen(PORT, HOST);
